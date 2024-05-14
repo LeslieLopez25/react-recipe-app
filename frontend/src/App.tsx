25f2280dc3ea92abaa965b63c429247d83a5b7f5
Leslie Lopez <leslie2515lopez@gmail.com>
2024-05-13 23:12:21 -0600

    Display search results on UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,36 @@
+import { FormEvent, useState } from "react";
+import * as api from "./api";
+import { Recipe } from "./types";
+
 import "./App.css";
 
 const App = () => {
-  return <div>Hello from recipe app!</div>;
+  const [searchTerm, setSearchTerm] = useState("burgers");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <button type="submit">Submit</button>
+      </form>
+      {recipes.map((recipe, id) => (
+        <div key={id}>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
