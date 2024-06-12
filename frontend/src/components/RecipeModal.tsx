b67fa420d6618ea51905de509654023e11b216bc
Leslie Lopez <leslie2515lopez@gmail.com>
2024-06-11 23:59:02 -0600

    Summary modal and summary endpoint creation

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+import { RecipeSummary } from "../types";
+import * as.RecipeAPI from "../api"
+
 const RecipeModal = () => {
+  const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+
+  useEffect(() => {
+    const fetchRecipeSummary = async () => {
+    try {
+      const summaryRecipe = await getRecipeSummary(recipeId)
+      setRecipeSummary(summaryRecipe)
+    } catch (error) {
+      console.log(error)
+    }
+    }
+    
+    fetchRecipeSummary()
+})
+
+  if (!recipeSummary) {
+    return <></>;
+  }
+
   return (
     <>
       <div className="overlay"></div>
       <div className="modal">
         <div className="modal-content">
           <div className="modal-header">
-            <h2>RECIPE TITLE</h2>
+            <h2>{recipeSummary?.title}</h2>
             <span className="close-btn">&times;</span>
           </div>
-          <p>RECIPE SUMMARY</p>
+          <p dangerouslySetInnerHTML={{ __html: recipeSummary.summary }}></p>
         </div>
       </div>
     </>
