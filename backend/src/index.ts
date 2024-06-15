160a14922d021e155849d60b0e46796af2db47d4
Leslie Lopez <leslie2515lopez@gmail.com>
2024-06-14 23:54:50 -0600

    Adding favorites endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import * as RecipeAPI from "./recipe-api";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prismaClient = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +22,22 @@ app.get("/api/recipes/:recipeId/summary", async (req, res) => {
   const recipeId = req.params.recipeId;
   const results = await RecipeAPI.getRecipeSummary(recipeId);
   return res.json(results);
+});
+
+app.post("/api/recipes/favorite", async (req, res) => {
+  const recipeId = req.body.recipeId;
+
+  try {
+    const favoriteRecipe = await prismaClient.favoriteRecipes.create({
+      data: {
+        recipeId: recipeId,
+      },
+    });
+    return res.status(201).json(favoriteRecipe);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Oops, something went wrong" });
+  }
 });
 
 app.listen(5000, () => {
